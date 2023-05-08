4d629083d1ea3fe83a207a33c25a5939dbc144bd
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-08 22:12:23 +0100

    Adding dynamic data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to Sabirah's React blog!";
+  const github = "https://github.com/Sabirah42"
+
+  // rendered JSX template is returned below
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+
+        <p>My self-esteem rating today: { Math.floor(Math.random() * 10) }</p>
+
+        <p>Please check out my Github account <a href={ github }>here</a>.</p>
       </div>
     </div>
   );
