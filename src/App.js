91e4ce78826da60d350d3668ab8c6aefbb1dcdec
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-09 14:04:21 +0100

    added Navbar and Home components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
+
+
+// App is the root application rendered first. Future components should
+// be nested wthin this to make a component tree
 function App() {
   const title = "Welcome to Sabirah's React blog!";
   const github = "https://github.com/Sabirah42"
@@ -7,12 +13,12 @@ function App() {
   // rendered JSX template is returned below
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
+        <Home />
+          <p>My self-esteem rating today: { Math.floor(Math.random() * 10) }</p>
 
-        <p>My self-esteem rating today: { Math.floor(Math.random() * 10) }</p>
-
-        <p>Please check out my Github account <a href={ github }>here</a>.</p>
+          <p>Please check out my Github account <a href={ github }>here</a>.</p>
       </div>
     </div>
   );
