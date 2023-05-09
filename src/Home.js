7d1cd94f13df50d571c55abc8a8c295b52fc7f71
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-09 17:03:24 +0100

    Added blog entry previews and styling to homepage

--- src/Home.js
+++ src/Home.js
@@ -11,15 +11,30 @@ const Home = () => {
         setDream('Turn on the heating without worrying.');
     }
 
+    const [entries, setEntries] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Sabirah', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Sabirah', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Sabirah', id: 3 }
+    ])
+
     return (
         <div className="home">
-            <h2>{ title }</h2>
-            <p>My self-esteem rating today: { Math.floor(Math.random() * 10) }</p>
-            <p>Please check out my Github account <a href={ github }>here</a>.</p>
+            <h2>{ title }</h2><br />
+            <p>My self-esteem rating today: { Math.floor(Math.random() * 10) }</p><br />
+            <p>Please check out my Github account <a href={ github }>here</a>.</p><br />
             <p>My lifelong dream: { dream }</p>
-            Or <button onClick={handleClick}>click me</button> to change my dreams.
-
-          </div>
+            <button onClick={handleClick}>Click me</button> to change my dreams.
+            
+            <div>
+                {/* cycle through each entry - MUST have a key to allow React to distinguish between each iteration */}
+                {entries.map((entry) => (
+                    <div className="entry-preview" key={ entry.id }>
+                        <h3>{ entry.title }</h3>
+                        <p>Written by { entry.author }</p>
+                    </div>
+                ))}
+            </div>
+        </div>
 
     );
 }
