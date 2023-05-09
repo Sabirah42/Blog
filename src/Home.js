2c6acf59623d129d5bd325e3e033297ff31155db
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-09 15:29:58 +0100

    Tidied structure and added dream Hook

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,26 @@
+import {useState } from 'react';
+
 const Home = () => {
 
     const title = "Welcome to Sabirah's React blog!";
+    const github = "https://github.com/Sabirah42"
 
+    const [dream, setDream] = useState('Have fur babies.')
+
+    const handleClick = () => {
+        setDream('Turn on the heating without worrying.');
+    }
 
     return (
         <div className="home">
             <h2>{ title }</h2>
-        </div>
+            <p>My self-esteem rating today: { Math.floor(Math.random() * 10) }</p>
+            <p>Please check out my Github account <a href={ github }>here</a>.</p>
+            <p>My lifelong dream: { dream }</p>
+            Or <button onClick={handleClick}>click me</button> to change my dreams.
+
+          </div>
+
     );
 }
  
