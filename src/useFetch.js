3eb185b1cdc25ceaf0b5ad1f1766d0496f4201aa
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-11 19:41:15 +0100

    added fetch abort

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,18 +7,23 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-    // FETCHing data from an end-point. This is a GET request to that URL, which returns a promise
-        fetch(url)
-    // Once the data is returned i.e. the RESponse, THEN you extract the data by returning res.json
+// AbortController aborts a fetch request when a user interrupts the request, for example, by
+// clicking off the page. This is anchored to the specific fetch request using the second param
+// in 'fetch' below.
+        const abortCont = new AbortController();
+
+// FETCHing data from an end-point. This is a GET request to that URL, which returns a promise
+        fetch(url, { signal: abortCont.signal })
+// Once the data is returned i.e. the RESponse, THEN you extract the data by returning res.json
         .then(res => {
             if(!res.ok) {
                 throw Error('Could not fetch blog data.');
             }
             return res.json();
         })
-    // THEN you can fire off another function that uses the returned DATA and change the state
-    // from null.
-    // This 'data' is only returned within this function. Different to data set on line 5.
+// THEN you can fire off another function that uses the returned DATA and change the state
+// from null.
+// This 'data' is only returned within this function. Different to data set on line 5.
         .then((data) => {
             console.log(data);
             setData(data);
@@ -26,16 +31,22 @@ const useFetch = (url) => {
             setError(null);
         })
         .catch(err => {
+            if (err.name === 'AbortError') {
+                console.log('fetch request aborted')
+            } else {
             setIsPending(false)
             setError(err.message);
+            }
         })
+// This function needs to be returned for abort to work
+        return () => abortCont.abort();
     }, [url]);
 
-    // an empty dependency array passed in as a second argument will mean useEffect only fires
-    // once on initial render, not after every state change.
-    // If a dependency is added e.g. 'entries' or 'dream', then React will watch for any changes
-    // to these dependencies and fire on changes.   
-    
+// an empty dependency array passed in as a second argument will mean useEffect only fires
+// once on initial render, not after every state change.
+// If a dependency is added e.g. 'entries' or 'dream', then React will watch for any changes
+// to these dependencies and fire on changes.   
+
     return { data, isPending, error }
 };
 
