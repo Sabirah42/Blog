b43a2e39951dfbd553f1e2b199a4bfad140b513c
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-11 19:13:41 +0100

    moved from href to Link in Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Blog</h1>
+{/* Using Link rather than an href stops React from sending a new request to the server on 
+    each click; instead, React intercepts the request and renders the Linked component directly
+    in the browser [much quicker] */}
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Entry</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Entry</Link>
             </div>
         </nav>
     );
