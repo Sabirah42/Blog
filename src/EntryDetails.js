46a3cc81b6deda231ce244b2c648d5f26c5db3ad
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-12 18:21:49 +0100

    Added content to individual entries

--- src/EntryDetails.js
+++ src/EntryDetails.js
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const EntryDetails = () => {
     // This is the changeable element of the entry details route (':id')
     const { id } = useParams();
-
+    // The word after the colon defines what 'data' is in this component (could be left as 'data'
+    // and used as such here as well)
+    const { data: entry, error, isPending } = useFetch('http://localhost:8000/entries/' + id)
     return (
         <div className="entry-details">
-            <h2>Entry Details - { id }</h2>
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div>}
+            { entry && (
+                <article>
+                    <h2>{ entry.title }</h2>
+                    <p>Written by { entry.author }</p>
+                    <div>{ entry.body }</div>
+                </article>
+            )}
         </div>
     );
 }
