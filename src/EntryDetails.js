80c25cb46a02e1078b9c322d9a1396f4cada0ae7
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-12 18:08:42 +0100

    linked entries on homepage to indivdual blog entries

--- src/EntryDetails.js
+++ src/EntryDetails.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const EntryDetails = () => {
+    // This is the changeable element of the entry details route (':id')
+    const { id } = useParams();
+
     return (
         <div className="entry-details">
-            <h2>Entry Details</h2>
+            <h2>Entry Details - { id }</h2>
         </div>
     );
 }
--- src/EntryList.js
+++ src/EntryList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const EntryList = (props) => {
     // pass in the entries prop from the Home page
     const entries = props.entries;
@@ -9,8 +11,10 @@ const EntryList = (props) => {
         {/* cycle through each entry - MUST have a key to allow React to distinguish between each iteration */}
             {entries.map((entry) => (
             <div className="entry-preview" key={ entry.id }>
-                <h3>{ entry.title }</h3>
-                <p>Written by { entry.author }</p>
+                <Link to={`/entries/${entry.id}`}>
+                    <h3>{ entry.title }</h3>
+                    <p>Written by { entry.author }</p>
+                </Link>
             </div>
         ))}
         </div>
