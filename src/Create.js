7a45c993073b6532991aef6898162d650437a7e7
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-16 17:21:49 +0100

    Added new blog entry form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a new blog entry</h2>
+            <form>
+                <label>Blog entry title:</label>
+                <input
+                    type="text"
+                    required
+                />
+                <label>Blog entry content:</label>
+                <textarea
+                    required
+                ></textarea>
+                <label>Author:</label>
+                <select>
+                    <option value="Sabirah">Sabirah</option>
+                    <option value="Not Sabirah">Not Sabirah?</option>
+                </select>
+                <button>Add entry</button>
+            </form>
         </div>
     );
 }
