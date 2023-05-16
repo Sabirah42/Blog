3ad359c6125d40b3e741e26fa33df878e26ac391
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-16 18:19:21 +0100

    Set up POST endpoint on new entry submission

--- src/Create.js
+++ src/Create.js
@@ -1,19 +1,50 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Sabirah');
+
+    const handleSubmit = (e) => {
+        // Stops page from refreshing on form submission, as this will lose the typed data
+        e.preventDefault();
+        // id properly is not needed for the entry object, as the JSON server will add this
+        const entry = { title, body, author };
+
+        fetch('http://localhost:8000/entries', {
+            // second argument defines the type of request we're sending,
+            method: 'POST',
+            // tells the server the type of content we're sending with this request,
+            headers: { "Content-Type": 'application/json'},
+            // and converts the data we're sending into a readable JSON string
+            body: JSON.stringify(entry)
+        }).then(() => {
+            console.log('New entry added');
+        })
+    }
+
     return (
         <div className="create">
             <h2>Add a new blog entry</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog entry title:</label>
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog entry content:</label>
                 <textarea
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="Sabirah">Sabirah</option>
                     <option value="Not Sabirah">Not Sabirah?</option>
                 </select>
